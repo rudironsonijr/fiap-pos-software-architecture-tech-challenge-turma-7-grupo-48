c60a7b445da2c9bdda44431dd1d21fb918a1f940
Bruno Palodetto <bruno.palodetto@animaeducacao.com.br>
2024-05-01 21:53:19 -0300

    [fix] making EntityArgumentNumberInvalidException constast Private

--- src/Domain/Entities/Exceptions/EntityArgumentNumberInvalidException.cs
+++ src/Domain/Entities/Exceptions/EntityArgumentNumberInvalidException.cs
@@ -2,10 +2,10 @@ namespace Domain.Entities.Exceptions
 {
 	public class EntityArgumentNumberInvalidException : Exception
 	{
-		public const string DefaultNumberInvalidGreaterOrEqualMessageTemplate =
+		private const string DefaultNumberInvalidGreaterOrEqualMessageTemplate =
 			"The property {0} can't be greater than or equal {1} in model {2}";
 
-		public const string DefaultNumberInvalidLessMessageTemplate =
+		private const string DefaultNumberInvalidLessMessageTemplate =
 			"The property {0} can't be less than or equal {1} in model {2}";
 
 		private EntityArgumentNumberInvalidException(
